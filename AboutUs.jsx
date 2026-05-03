428510595a79811ce0dd15fa62405487ef0362bb
lucduong232 <dl2322005@gmail.com>
2026-05-03 08:17:24 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>Về Công ty của chúng tôi</h1>
+      <p>Tên công ty: Công ty Công nghệ ABC</p>
+      <p>Địa chỉ: Hà Nội, Việt Nam</p>
+      <p>Sứ mệnh: Cung cấp giải pháp phần mềm tốt nhất cho khách hàng.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
